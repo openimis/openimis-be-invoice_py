baf9dff3d752c55aa01211d0e9c0952aa37aa70c
sniedzielski <sniedzielski@soldevelo.com>
2022-05-16 11:30:19 +0200

    OP-503: added service to add Payment with PaymentDetails

--- invoice/services/paymentInvoice.py
+++ invoice/services/paymentInvoice.py
@@ -1,8 +1,18 @@
 from django.db import transaction
 
 from core.services import BaseService
-from core.services.utils import check_authentication, output_exception, output_result_success, model_representation
-from invoice.models import PaymentInvoice, Invoice, DetailPaymentInvoice
+from core.services.utils import (
+    check_authentication,
+    output_exception,
+    output_result_success,
+    model_representation
+)
+from invoice.models import (
+    Bill,
+    PaymentInvoice,
+    Invoice,
+    DetailPaymentInvoice
+)
 from invoice.utils import resolve_payment_details
 from invoice.validation.paymentInvoice import PaymentInvoiceModelValidation
 
@@ -20,8 +30,22 @@ class PaymentInvoiceService(BaseService):
         raise NotImplementedError("Update method is not implemented for PaymentInvoice")
 
     @check_authentication
-    def create(self, obj_data):
-        raise NotImplementedError("Create method is not implemented for PaymentInvoice")
+    def create_with_detail(self, payment_invoice, payment_detail):
+        try:
+            with transaction.atomic():
+                payment = PaymentInvoice(**payment_invoice)
+                payment.save(username=self.user.username)
+                payment_detail.payment = payment
+                payment_detail.subject = self._get_generic_object(
+                    payment_detail.subject_id,
+                    payment_detail.subject_type
+                )
+                payment_detail.save(username=self.user.username)
+                dict_repr = model_representation(payment)
+                dict_repr['payment_detail_uuid'] = payment_detail.uuid
+                return output_result_success(dict_representation=dict_repr)
+        except Exception as exc:
+            return output_exception(model_name="PaymentInvoice", method="create_with_detail", exception=exc)
 
     @check_authentication
     def ref_received(self, payment_invoice: PaymentInvoice, payment_ref):
@@ -85,3 +109,25 @@ class PaymentInvoiceService(BaseService):
             if detail.status != status:
                 detail.status = status
                 detail.save(username=self.user.username)
+
+    @classmethod
+    def _get_field_for_detail(cls, data):
+        status = data.pop('status')
+        subject_id = data.pop('subject_id')
+        subject_type = data.pop('subject_type')
+        return status, subject_id, subject_type
+
+    @classmethod
+    def _create_payment_detail(cls, user, data, payment, status, subject_id, subject_type):
+        payment_detail = cls._build_payment_detail(data, payment, status, subject_id, subject_type)
+        detail_payment_invoice = DetailPaymentInvoice(**payment_detail)
+        detail_payment_invoice.save(username=user.username)
+
+    @classmethod
+    def _get_generic_object(cls, uuid, type):
+        if type.model == 'invoice':
+            object = Invoice.objects.get(id=uuid)
+        else:
+            object = Bill.objects.get(id=uuid)
+        return object
+
