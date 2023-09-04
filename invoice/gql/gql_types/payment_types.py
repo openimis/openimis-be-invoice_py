b31e6a89380fa88a8a90b351a5669e028a268329
Patrick Delcroix <delcroip@gmail.com>
2023-09-04 16:10:07 +0200

    MERGING RELEASE branches (#47)
    * OTC:734: Added export
    
    * OTC-734: Removed unused import
    
    * OP-1088: Missing migration added.
    
    * OTC-878: add all permission to gql mutations and queries
    
    * OTC-878: update job
    
    ---------
    
    Co-authored-by: Damian Borowiecki <dborowiecki@soldevelo.com>
    Co-authored-by: wzglinieckisoldevelo <wzgliniecki@soldevelo.com>
    Co-authored-by: jdolkowski <jdolkowski@soldevelo.com>
    Co-authored-by: Dragos DOBRE <dragos.dobre@swisstph.ch>

--- invoice/gql/gql_types/payment_types.py
+++ invoice/gql/gql_types/payment_types.py
@@ -5,9 +5,12 @@ from django.core.serializers.json import DjangoJSONEncoder
 from graphene_django import DjangoObjectType
 
 from core import prefix_filterset, ExtendedConnection
+from invoice.apps import InvoiceConfig
 from invoice.gql.filter_mixin import GenericFilterGQLTypeMixin
 from invoice.models import PaymentInvoice, DetailPaymentInvoice
 from invoice.utils import underscore_to_camel
+from django.utils.translation import gettext as _
+from django.core.exceptions import PermissionDenied
 
 
 class PaymentInvoiceGQLType(DjangoObjectType, GenericFilterGQLTypeMixin):
@@ -30,14 +33,20 @@ class DetailPaymentInvoiceGQLType(DjangoObjectType, GenericFilterGQLTypeMixin):
 
     subject_type = graphene.Int()
     def resolve_subject_type(root, info):
+        if not info.context.user.has_perms(InvoiceConfig.gql_invoice_payment_search_perms):
+            raise PermissionDenied(_("unauthorized"))
         return root.subject_type.id
 
     subject_type_name = graphene.String()
     def resolve_subject_type_name(root, info):
+        if not info.context.user.has_perms(InvoiceConfig.gql_invoice_payment_search_perms):
+            raise PermissionDenied(_("unauthorized"))
         return root.subject_type.name
 
     subject = graphene.JSONString()
     def resolve_subject(root, info):
+        if not info.context.user.has_perms(InvoiceConfig.gql_invoice_payment_search_perms):
+            raise PermissionDenied(_("unauthorized"))
         subject_object_dict = root.subject.__dict__
         subject_object_dict.pop('_state')
         subject_object_dict = {
