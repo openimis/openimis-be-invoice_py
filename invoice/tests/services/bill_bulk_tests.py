c43e3768e9aaab869723e75183e046104aa7655f
Anthony BELEM <anthbel@gmail.com>
2026-03-23 17:00:27 +0200

    refactor: code refactoring

--- invoice/tests/services/bill_bulk_tests.py
+++ invoice/tests/services/bill_bulk_tests.py
@@ -1,21 +1,18 @@
 import uuid
 from django.test import TestCase
-from django.contrib.contenttypes.models import ContentType
-from core.models import User
 from core.test_helpers import LogInHelper
 from invoice.models import Bill, BillItem
 from invoice.services import BillService
 
 class BillBulkTests(TestCase):
     @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+    def setUpTestData(cls):
         cls.user = LogInHelper().get_or_create_user_api(username='admin_bulk')
 
     def test_bulk_create_bills_success(self):
         """Verify that bulk_create_bills correctly persists multiple Bill instances."""
         bill_instances = []
-        
+
         for i in range(5):
             bill_instances.append(Bill(
                 id=uuid.uuid4(),
@@ -27,7 +24,7 @@ class BillBulkTests(TestCase):
                 user_updated=self.user,
                 version=1
             ))
-            
+
         created_bills = BillService.bulk_create_bills(bill_instances)
         self.assertEqual(len(created_bills), 5)
         self.assertEqual(Bill.objects.filter(code__startswith="BULK_BILL_").count(), 5)
@@ -44,7 +41,7 @@ class BillBulkTests(TestCase):
             version=1
         )
         bill.save(username=self.user.username)
-        
+
         item_instances = []
         for i in range(10):
             item_instances.append(BillItem(
@@ -56,7 +53,7 @@ class BillBulkTests(TestCase):
                 user_updated=self.user,
                 version=1
             ))
-            
+
         created_items = BillService.bulk_create_bill_items(item_instances)
         self.assertEqual(len(created_items), 10)
         self.assertEqual(BillItem.objects.filter(bill=bill).count(), 10)
